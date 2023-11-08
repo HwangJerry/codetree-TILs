5c99c47d7ed17ccf3ba9d3eb4e6ad02ef59ee9dc
zackary hwang <99382672+HwangBaco@users.noreply.github.com>
2023-11-08 14:18:34 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231108/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 231108/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -32,24 +32,30 @@ public class Main {
             }
         }
         dp[0][0] = arr[0][0];
-
-        // init
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = Math.min(dp[0][i - 1], arr[0][i]);
-        }
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][0], arr[i][0]);
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (i == n - 1 && j == n - 1) {
-                    break;
-                }
-                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]);
+        if (n == 1) {
+            System.out.println(dp[0][0]);
+        } else {
+            // init
+            for (int i = 1; i < n; i++) {
+                dp[0][i] = Math.min(dp[0][i - 1], arr[0][i]);
             }
-        }
-        dp[n - 1][n - 1] = Math.max(dp[n - 2][n - 1], dp[n - 1][n - 2]);
-        System.out.println(dp[n-1][n-1]);
+            for (int i = 1; i < n; i++) {
+                dp[i][0] = Math.min(dp[i - 1][0], arr[i][0]);
+            }
+    
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < n; j++) {
+                    if (i == n - 1 && j == n - 1) {
+                        break;
+                    }
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+            dp[n - 1][n - 1] = Math.max(dp[n - 2][n - 1], dp[n - 1][n - 2]);
+            System.out.println(dp[n-1][n-1]);
+            
+        } 
+
+
     }
 }
