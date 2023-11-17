4e70ffeb13b90fc67bedbf545f6a70a37a8241ec
zackary hwang <99382672+HwangBaco@users.noreply.github.com>
2023-11-17 12:54:31 +0900

    Title: 2차원 최대 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231117/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n, m;
+    private static int[][] arr;
+    private static int[][] dp;
+
+    private static Queue<Integer> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // init
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+            dp[0][i] = 1;
+        }
+
+        // tabulation
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                for (int k = 0; k < i; k++) {
+                    for (int l = 0; l < j; l++) {
+                        if (arr[i][j] > arr[k][l]) {
+                            dp[i][j] = Math.max(dp[i][j], dp[k][l] + 1);
+                            pq.add(-dp[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(-pq.poll());
+    }
+}
