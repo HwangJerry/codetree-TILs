a4d67ac2f0a6922b661f79a9e4f053c24e57b20f
zackary hwang <99382672+HwangBaco@users.noreply.github.com>
2023-11-17 13:10:33 +0900

    Title: 2차원 최대 증가 수열, Time: 148ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 231117/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 231117/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -36,9 +36,20 @@ public class Main {
             dp[0][i] = 1;
         }
 
-        // tabulation
         for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+            if (arr[i][1] > arr[0][0]) {
+                dp[i][1] = 2;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (arr[1][i] > arr[0][0]) {
+                dp[1][i] = 2;
+            }
+        }
+
+        // tabulation
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j < m; j++) {
                 for (int k = 0; k < i; k++) {
                     for (int l = 0; l < j; l++) {
                         if (arr[i][j] > arr[k][l]) {
