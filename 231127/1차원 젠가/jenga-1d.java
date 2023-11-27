5d17c5287422fc78472e2ddac3bacd893c6cf67f
zackary hwang <ghkdwp018@gmail.com>
2023-11-27 11:29:53 +0900

    Title: 1차원 젠가, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231127/1차원 젠가/jenga-1d.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*
+        * 블록 길이 n 입력
+        * 길이 n에 맞는 블록 입력
+        * 구간에 맞게 블록 빼기
+        * 그리고 나서 남은 블록의 개수와 남은 블록 출력
+        * */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> blocks = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            blocks.add(Integer.parseInt(br.readLine()));
+        }
+
+        StringTokenizer st;
+
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startIdx = Integer.parseInt(st.nextToken()) - 1;
+            int endIdx = Integer.parseInt(st.nextToken()) - 1;
+            for (int j = 0; j <= (endIdx - startIdx); j++) {
+                blocks.remove(startIdx);
+            }
+
+        }
+        System.out.println(blocks.size());
+        blocks.forEach(System.out::println);
+    }
+}
