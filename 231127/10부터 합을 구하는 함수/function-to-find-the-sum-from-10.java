e407513eb5c238a366d1e0a933c3997858d4c4a7
zackary hwang <ghkdwp018@gmail.com>
2023-11-27 12:48:30 +0900

    Title: 10부터 합을 구하는 함수, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231127/10부터 합을 구하는 함수/function-to-find-the-sum-from-10.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
+        for (int i = 10; i <= n; i++) {
+            ans += i;
+        }
+        System.out.println(ans);
+    }
+}
